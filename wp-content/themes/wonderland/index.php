457b3db252532a088e7d24fd3956910d9ab71025
marcos-sco <marcos_sco@outlook.com>
2026-03-21 23:54:12 -0300

    Update hero section with new content, buttons, and responsive styles

--- wp-content/themes/wonderland/index.php
+++ wp-content/themes/wonderland/index.php
@@ -4,7 +4,35 @@
     <article>
         <section class="hero">
             <div class="hero-wrapper">
-                
+                <div class="consultation-announcement">
+                    <p><?= __('As a Court Appointed Expert Witness in Mold-Related Cases, We’re Trusted Across Washington, DC', 'wonderland') ?></p>
+                    <p class="yellow-primary"><?= __('Contact Us for a Free Consultation', 'wonderland') ?></p>
+                    
+                    <small><?= __('Let our expertise guide you. Schedule a consultation to determine if on-site mold testing is the next step for you.', 'wonderland') ?></small>
+                </div>
+
+                <div class="call-to-action">
+                    <a href="#" class="btn"><?= __('Schedule a visit', 'wonderland') ?></a>
+                    <a href="#" class="btn white"><?= __('Call Us Now!', 'wonderland') ?></a>
+                </div>
+
+                <div class="review">
+                    <div class="ellipse-icon">
+                        <?= $GLOBALS['svgLoader']->render('leaf_icon'); ?>
+                    </div>
+
+                    <div class="info">
+                        <div class="grade">
+                            <div class="stars"></div>
+                            <div class="score">4.95/5.00</div>
+                        </div>
+
+                        <div class="message">
+                            <p><?= __('Over 14.5k happy customers on', 'wonderland'); ?> <a href="#">Yelp</a></p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     </article>
