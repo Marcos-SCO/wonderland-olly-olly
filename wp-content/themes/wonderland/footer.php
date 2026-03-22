75a31dfb917bc7e25517d307cc01c6e180ac25b5
marcos-sco <marcos_sco@outlook.com>
2026-03-22 20:01:32 -0300

    Update footer styles and add developer attribution link

--- wp-content/themes/wonderland/footer.php
+++ wp-content/themes/wonderland/footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <p>© <?php echo date('Y'); ?></p>
+  <p>© <a href="https://github.com/Marcos-SCO" target="_blank" rel="noopener noreferrer">Develop by Marcos-SCO</a> | <?php echo date('Y'); ?></p>
 </footer>
 
 <?php wp_footer(); ?>
